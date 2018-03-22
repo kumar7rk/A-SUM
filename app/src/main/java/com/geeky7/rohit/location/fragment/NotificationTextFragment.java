879c978057b593108289355ebca527550b6784b9
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-03-22 22:58:04 +1030

    NotificationDialog: neutral button added which reset the text and does not close the dialog :pray:
    next step to show a dialog defore resetting

--- app/src/main/java/com/geeky7/rohit/location/fragment/NotificationTextFragment.java
+++ app/src/main/java/com/geeky7/rohit/location/fragment/NotificationTextFragment.java
@@ -11,9 +11,12 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.geeky7.rohit.location.CONSTANTS;
 import com.geeky7.rohit.location.R;
@@ -52,16 +55,17 @@ public class NotificationTextFragment extends DialogFragment {
         //putting the caret on the last of the keyword for convenience
         notification_text_et.setSelection(notification_text_et.getText().length());
 
-        String text = notification_text_et.getText().toString();
-        if (text.equals(""))
-            notification_text_et.setText(CONSTANTS.NOTIFICATION_TEXT_DEFAULT);
+//        String text = notification_text_et.getText().toString();
 
         // creating dialog with buttons
         //on click save button save the keyword in the sharedPreferences
         alertDialog = new AlertDialog.Builder(getActivity())
-        .setTitle("Customise Notification Text")
-        .setView(inflater.inflate(R.layout.activity_notification_text, null))
-                // save button, onClick updates the keyword in sharedPreference
+            .setTitle("Customise Notification Text")
+            .setView(inflater.inflate(R.layout.activity_notification_text, null))
+            //.setPositiveButton(R.string.save,null)
+            //.setNegativeButton(R.string.close,null)
+            .setNeutralButton(R.string.reset,null)
+         // save button, onClick updates the keyword in sharedPreference
                 .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
@@ -76,28 +80,38 @@ public class NotificationTextFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int id) {
                     }
                 })
-        //share button to share the keyword via sms
-        // open the default messaging app with pre added text with keyword
-        .setNeutralButton(R.string.reset, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                notification_text_et.setText(CONSTANTS.NOTIFICATION_TEXT_DEFAULT);
-                return true;
-            }
-        });
+                //reset the text to default
+            .setNeutralButton(R.string.reset, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    notification_text_et.setText(CONSTANTS.NOTIFICATION_TEXT_DEFAULT);
+                }
+            });
         return alertDialog.create();
     }
-
     //gets the saved keyword from the sharedPreferences and also magically puts the caret at the end of the text
     // yeah it's not like that by default
     @Override
     public void onResume() {
         m.calledMethodLog(TAG,"onResume");
-
         super.onResume();
+
+        final AlertDialog dialog = (AlertDialog) getDialog();
+        if (dialog!=null){
+            Button resetB = dialog.getButton(Dialog.BUTTON_NEUTRAL);
+            resetB.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        m.updateLog(TAG,"Resetting everything in 3..2...1. Just kidding. Just resetting the notification text to default");
+                        notification_text_et.setText(CONSTANTS.NOTIFICATION_TEXT_DEFAULT);
+                        notification_text_et.setSelection(notification_text_et.getText().length());
+                    }
+            });
+
+        }
         notification_text_et = (EditText) getDialog().findViewById(R.id.notification_text_et);
-        // get the stored keyword
-        notification_text_et.setText(preferences.getString(CONSTANTS.NOTIFICATION_TEXT,""));
+        // get the stored notification text
+        notification_text_et.setText(preferences.getString(CONSTANTS.NOTIFICATION_TEXT,CONSTANTS.NOTIFICATION_TEXT_DEFAULT));
         // move the caret to the last of the text; front by default
         notification_text_et.setSelection(notification_text_et.getText().length());
     }
