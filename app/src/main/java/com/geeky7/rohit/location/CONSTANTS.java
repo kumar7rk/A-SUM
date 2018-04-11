d2396818c6e180f52d0cf1d9de45b7d89f7ed0a8
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-04-11 21:37:39 +0930

    CONSTANT class is written some nice comments and the text is categorised :information_source: :crocodile:

--- app/src/main/java/com/geeky7/rohit/location/CONSTANTS.java
+++ app/src/main/java/com/geeky7/rohit/location/CONSTANTS.java
@@ -4,44 +4,59 @@ package com.geeky7.rohit.location;
  * Created by Rohit on 6/09/2016.
  */
 public class CONSTANTS {
+    // numbers
     public static final String NUMBER_OF_SCENARIOS_SELECTED = "NUMBER_OF_SCENARIOS_SELECTED";
-    public static final String SELECTED_RULE = "SELECTED_RULE";
-    public static final String MAIN_SWITCH = "MAIN_SWITCH";
     public static final String NUMBER_OF_VIOLATIONS = "NUMBER_OF_VIOLATIONS";
+    public static final String NUMBER_OF_INSTALLED_APPS = "NUMBER_OF_INSTALLED_APPS";
+
+    //scenario and rule
+    public static final String SELECTED_RULE = "SELECTED_RULE";
     public static final String DETECTED_SCENARIO = "DETECTED_SCENARIO";
-    public static final String VIOLATION_TIME = "VIOLATION_TIME";
+
+    //idk
+    public static final String MAIN_SWITCH = "MAIN_SWITCH";
+
+    //rules added time
     public static final String AUTOMATIC_RULE_ADDED_TIME = "AUTOMATIC_RULE_ADDED_TIME";
     public static final String SEMIAUTOMATIC_RULE_ADDED_TIME = "SEMIAUTOMATIC_RULE_ADDED_TIME";
     public static final String MANUAL_RULE_ADDED_TIME = "MANUAL_RULE_ADDED_TIME";
     public static final String NOTIFICATION_RULE_ADDED_TIME = "NOTIFICATION_RULE_ADDED_TIME";
+
+
+    //other times
+    public static final String VIOLATION_TIME = "VIOLATION_TIME";
     public static final String SEMIAUTOMATIC_SEEKBAR_PROGRESS_OFF_TIME = "SEMIAUTOMATIC_SEEKBAR_PROGRESS_OFF_TIME";
 
+    //scenarios detected times
     public static final String DETECTED_RESTAURNT_TIME = "DETECTED_RESTAURNT_TIME";
     public static final String DETECTED_RELIGIOUSPLACE_TIME = "DETECTED_RELIGIOUSPLACE_TIME";
     public static final String DETECTED_MOVIETHEATRE_TIME = "DETECTED_MOVIETHEATRE_TIME";
     public static final String DETECTED_BEDANDDARK_TIME = "DETECTED_BEDANDDARK_TIME";
     public static final String DETECTED_WALKING_TIME = "DETECTED_WALKING_TIME";
 
+    //activities
     public static final String MAIN_ACTIVITY = "MAIN_ACTIVITY";
 
+    //adapters
+    public static final String APPLICATION_ADAPTER = "APPLICATION_ADAPTER";
+
+    //fragments
     public static final String INSTALLED_APPS_DIALOG = "INSTALLED_APPS_DIALOG";
     public static final String MANUAL_RULE_FRAGMENT = "MANUAL_RULE_FRAGMENT";
+    public static final String NOTIFICATION_TEXT_FRAGMENT = "NOTIFICATION_TEXT_FRAGMENT";
 
+    //services
     public static final String BACKGROUND_SERVICE = "BACKGROUND_SERVICE";
     public static final String BED_AND_DARK_SERVICE = "BED_AND_DARK_SERVICE";
 
+    //classes
     public static final String MAIN = "MAIN";
 
-    public static final String APPLICATION_ADAPTER = "APPLICATION_ADAPTER";
-
-
+    //unGrouped
     public static final String APP_OPENED_FIRST_TIME = "APP_OPENED_FIRST_TIME";
 
-    public static final String NOTIFICATION_TEXT = "NOTIFICATION_TEXT";
+    //notification rule
     public static final String NOTIFICATION_TEXT_DEFAULT = MyApplication.getAppContext().getString(R.string.notification_text_default);
 
-    public static final String NUMBER_OF_INSTALLED_APPS = "NUMBER_OF_INSTALLED_APPS";
-
-
-
+    
 }
